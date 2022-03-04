fa8768cd465ab730802598667e0fe25fcfba4c8b
6981844 <j.g.vanboven@students.uu.nl>
2022-03-04 16:24:46 +0100

    feature selection in the main added

--- code/main.py
+++ code/main.py
@@ -21,6 +21,9 @@ def main(argv=None):
                     argv[3]: number of datarows to consider in training and testing, can be used to train on smaller portions of the data (e.g. to test the system)
                             this should be an int. If all rows should be considered, the input should be 'all'
                     
+                    optional:
+                    argv[4] : a string of selected features (separated by spaces), to test a combination of features different from the settings
+                    
     '''
     #picking up commandline arguments
     if argv is None:
@@ -32,6 +35,13 @@ def main(argv=None):
     trainingfile = argv[1]
     testfile = argv[2]
     n_rows = argv[3]
+
+    #if the features to use for argument prediction are defined, extract them
+    try:
+        selected_features = argv[4].split()
+        print(f"features that will be considered in argument prediction are : {selected_features}")
+    except:
+        selected_features = None
 
     #create paths to the output files in which the features and predictions should be saved
     train_features_output_path = trainingfile.replace('.conll','_features.conll')
@@ -53,7 +63,7 @@ def main(argv=None):
 
     #train argument classifier, create predictions on the test data and evaluate
     print('argument prediction')
-    argument_prediction.main(['', train_features_output_path, test_features_output_path, argument_predictions_output_path, n_rows])
+    argument_prediction.main(['', train_features_output_path, test_features_output_path, argument_predictions_output_path, n_rows, selected_features])
     print('argument evaluation')
     evaluate.main(['', argument_predictions_output_path, GOLD_INDEX, PRED_INDEX])
 
